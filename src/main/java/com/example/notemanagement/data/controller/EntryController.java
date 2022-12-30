53d7604a70fa2528ea51cf0d8b35b22c04df48e0
oladejomubarak <oladejomubarakade@gmail.com>
2022-12-30 10:23:06 +0000

    entry controller completed

--- src/main/java/com/example/notemanagement/data/controller/EntryController.java
+++ src/main/java/com/example/notemanagement/data/controller/EntryController.java
@@ -1,13 +1,12 @@
 package com.example.notemanagement.data.controller;
 
 import com.example.notemanagement.data.dtos.request.CreateEntriesRequest;
+import com.example.notemanagement.data.dtos.request.EntriesUpdateRequest;
 import com.example.notemanagement.services.EntriesServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EntryController {
@@ -18,5 +17,20 @@ private EntriesServices entriesServices;
     public ResponseEntity<?> createEntries(@RequestBody CreateEntriesRequest createEntriesRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(entriesServices.createEntries(createEntriesRequest));
     }
-
+    @PatchMapping("/updateentry")
+    public ResponseEntity<?> updateEntries(@RequestBody EntriesUpdateRequest entriesUpdateRequest){
+        return ResponseEntity.ok(entriesServices.updateEntries(entriesUpdateRequest));
+    }
+    @GetMapping("/viewentry/{id}")
+    public ResponseEntity<?> viewEntryById(@PathVariable int id){
+        return ResponseEntity.ok(entriesServices.viewEntryById(id));
+    }
+    @DeleteMapping("/deleteentry/{id}")
+    public ResponseEntity<?> deleteEntryById(@PathVariable int id){
+        return ResponseEntity.ok(entriesServices.deleteEntryById(id));
+    }
+    @DeleteMapping("deleteallentries")
+    public ResponseEntity<?> deleteAllEntries(){
+        return ResponseEntity.ok(entriesServices.deleteAllEntries());
+    }
 }
